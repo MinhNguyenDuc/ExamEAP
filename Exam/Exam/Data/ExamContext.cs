f3310c36cb0607660bd8579f8f40eb15c49e8538
Minh Nguyen Duc <nguyenducminh24101998@gmail.com>
2019-01-08 21:28:13 +0700

    new push

---
+++ Exam/Exam/Data/ExamContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Exam.Entity;
+
+namespace Exam.Models
+{
+    public class ExamContext : DbContext
+    {
+        public ExamContext (DbContextOptions<ExamContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Exam.Entity.Currency> Currency { get; set; }
+    }
+}
---
+++ Exam/Exam/Entity/Currency.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exam.Entity
+{
+    public class Currency
+    {
+        [Key]
+        public string Id { get; set; }
+        public int Ratio { get; set; }
+    }
+}
